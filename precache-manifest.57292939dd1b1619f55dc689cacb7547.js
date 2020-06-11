5c0117f40eabb98bd08333b8a05f6ffa6e09e4a3
shrinivasshah <shrinivasshah05@gmail.com>
2020-06-11 17:58:27 +0530

    Updates

---
+++ precache-manifest.57292939dd1b1619f55dc689cacb7547.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d034368681b521a03a7c30918ab187f",
+    "url": "/my-weather-app/index.html"
+  },
+  {
+    "revision": "9c5a02f59e9078119fd8",
+    "url": "/my-weather-app/static/css/2.a02a7df7.chunk.css"
+  },
+  {
+    "revision": "ee48c6ba793e683d238e",
+    "url": "/my-weather-app/static/css/main.e40406bf.chunk.css"
+  },
+  {
+    "revision": "9c5a02f59e9078119fd8",
+    "url": "/my-weather-app/static/js/2.5f49fe5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-weather-app/static/js/2.5f49fe5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee48c6ba793e683d238e",
+    "url": "/my-weather-app/static/js/main.7121bbda.chunk.js"
+  },
+  {
+    "revision": "e59172b5fa83ffbda992",
+    "url": "/my-weather-app/static/js/runtime-main.ad789414.js"
+  },
+  {
+    "revision": "8feb4728a7a8254f06b00dd57e73e2ca",
+    "url": "/my-weather-app/static/media/123.8feb4728.jpg"
+  },
+  {
+    "revision": "5460670192dc0a5d46190dffdc1424b7",
+    "url": "/my-weather-app/static/media/fog.54606701.jpg"
+  },
+  {
+    "revision": "45278d1d8a5f9850b9e6ec2e51c36be3",
+    "url": "/my-weather-app/static/media/rain.45278d1d.jpg"
+  },
+  {
+    "revision": "a9f3e57c3b28420dea0d8db8847baf7c",
+    "url": "/my-weather-app/static/media/rain2.a9f3e57c.jpg"
+  },
+  {
+    "revision": "ebff82be68df898b00f4af74eab2ea54",
+    "url": "/my-weather-app/static/media/sleet.ebff82be.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/my-weather-app/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/my-weather-app/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/my-weather-app/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/my-weather-app/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/my-weather-app/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
